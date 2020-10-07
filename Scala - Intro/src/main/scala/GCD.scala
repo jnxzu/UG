c8061636a22d017e142624b31c93438035a822c1
J <janbielowka@gmail.com>
2020-10-07 18:37:24 +0200

    scala intro

---
+++ Scala - Intro/src/main/scala/GCD.scala
@@ -0,0 +1,13 @@
+object Main extends App {
+  def prime(a: Int): Boolean = {
+    if (a <= 1) false
+    else if (a == 2) true
+    else !(2 to (a - 1)).exists(x => a % x == 0)
+  }
+
+  print("Input your number: ")
+  val a = io.StdIn.readInt()
+  val result = prime(a)
+  println()
+  println(s"It is $result that your number $a is prime.")
+}
---
+++ Scala - Intro/src/main/scala/Prime.scala
@@ -0,0 +1,13 @@
+object Main extends App {
+  def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
+  }
+
+  print("Input first number: ")
+  val a = io.StdIn.readInt()
+  println()
+  print("Input second number: ")
+  val b = io.StdIn.readInt()
+  val result = gcd(a, b)
+  println(s"$a's and $b's greatest common divisor is: $result")
+}
